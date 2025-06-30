665f8f54cba37616859138f2685147bbaa476826
vibincholayil <vibinnath.c@gmail.com>
2025-06-30 14:06:11 +0100

    updated terraform

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,30 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = "example"
+  cluster_version = "1.31"
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = "vpc-1234556abcdef"
+  subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
 /*
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
