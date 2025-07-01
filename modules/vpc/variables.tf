878731cbb30725b60bb801e371ef9c0b732157a9
vibincholayil <vibinnath.c@gmail.com>
2025-07-01 22:26:24 +0100

    Initial commit for e-commerce microservice project

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default     = "eu-west-2"
+  default     = "eu-central-1"
 }
 
 variable "vpc_cidr" {
--- output.tf
+++ output.tf
@@ -1,14 +1,29 @@
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+  description = "The ID of the VPC"
+  value       = aws_vpc.vibin_vpc.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.myprivatesubnet.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.mypublicsubnet.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat_gateway.id
+}
+
+output "eip_allocation_id" {
+  description = "The Elastic IP allocation ID for NAT Gateway"
+  value       = aws_eip.igw_eip.id
 }
