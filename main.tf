dec9f2c7482ef12325e8f5fbf991369cfe271345
vibincholayil <vibinnath.c@gmail.com>
2025-07-02 22:31:41 +0100

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "vpc" {
-  source                = "./modules/vpc_module"
-  region                = var.region
-  vpc_cidr              = var.vpc_cidr
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  public_subnet_cidrs   = var.public_subnet_cidrs
+  source = "./modules/vpc"
+  region = var.region
+  vpc_cidr = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
+
--- output.tf
+++ output.tf
@@ -1,29 +1,9 @@
-output "vpc_id" {
+output "region" {
+  description = "The AWS region where the resources are created"
+  value       = var.region
+} 
+
+output "vpc_cidr" {
   description = "The ID of the VPC"
-  value       = aws_vpc.vibin_vpc.id
-}
-
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = aws_subnet.myprivatesubnet.id
-}
-
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = aws_subnet.mypublicsubnet.id
-}
-
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw.id
-}
-
-output "nat_gateway_id" {
-  description = "The ID of the NAT Gateway"
-  value       = aws_nat_gateway.nat_gateway.id
-}
-
-output "eip_allocation_id" {
-  description = "The Elastic IP allocation ID for NAT Gateway"
-  value       = aws_eip.igw_eip.id
+  value       = var.vpc_cidr
 }
